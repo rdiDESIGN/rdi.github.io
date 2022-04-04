3cd498253bb7f97b54076771dd6b41c239cf8494
rdiDESIGN <102971694+rdiDESIGN@users.noreply.github.com>
2022-04-05 01:19:03 +0800

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+console.log('custom js is running!');
+
+$("#hide").click(function(){
+    $('*[data-work="print"]').hide();
+});
+
+$("#show").click(function(){
+    $('*[data-work="print"]').show();
+});
+
+$("#xxx").click(function(){
+	console.log('xxx is working');
+    jQuery(document).ready(function() {
+        if (typeof html5Lightbox === "undefined") html5Lightbox = jQuery(".html5lightbox").html5lightbox()
+    })
+
+});
